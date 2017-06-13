f44998751b6191f01a8e15464a4ed428eb82d1d3
Vladislav Vaintroub <wlad@mariadb.com>
2017-06-13 18:23:52 +0000

    MDEV-11159 : proxy protocol support. Work in progress

--- sql/net_serv.cc
+++ sql/net_serv.cc
@@ -828,6 +828,188 @@ static my_bool my_net_skip_rest(NET *net, uint32 remain, thr_alarm_t *alarmed,
 #endif /* NO_ALARM */
 
 
+static int parse_proxy_protocol_v1_header( char *hdr, size_t len,
+  struct sockaddr_storage *addr_storage, int *port, bool *local_connection)
+{
+  char *ctx = NULL;
+  char *token;
+  int   address_family;
+
+  *local_connection = false;
+  // Parse PROXY
+  token = strtok_r(hdr, " ", &ctx);
+  if (!token)
+    return -1;
+  if (strcmp(token, "PROXY"))
+    return -1;
+
+  // Parse address Family : TCP4, TCP6 or UNKNOWN
+  token = strtok_r(NULL, " ", &ctx);
+  if (!token)
+    return -1;
+
+  if (strcmp(token, "TCP4") == 0)
+    address_family = AF_INET;
+  else if (strcmp(token, "TCP6") == 0)
+    address_family = AF_INET6;
+  else if (strcmp(token, "UNKNOWN") == 0)
+    address_family = AF_UNSPEC;
+  else
+    return -1;
+
+  // Parse client IP address
+  token = strtok_r(NULL, " ", &ctx);
+  if (!token)
+    return -1;
+
+  void *addr;
+  if (address_family == AF_INET)
+    addr = &((struct sockaddr_in *)addr_storage)->sin_addr;
+  else
+    addr = &((struct sockaddr_in6 *)addr_storage)->sin6_addr;
+
+  addr_storage->ss_family = address_family;
+
+  if (!inet_pton(address_family, token, addr))
+    return -1;
+
+  // Parse server IP address
+  token = strtok_r(NULL, " ", &ctx);
+  if (!token)
+    return -1;
+
+  // Parse client port.
+  token = strtok_r(NULL, " ", &ctx);
+  *port = atoi(token);
+
+  if (address_family == AF_INET)
+    ((struct sockaddr_in *)addr_storage)->sin_port= (ushort) *port;
+  else
+    ((struct sockaddr_in6 *)addr_storage)->sin6_port = (ushort) *port;
+
+  // Parse server port
+  token = strtok_r(NULL, " ", &ctx);
+
+  return 0;
+}
+
+static int parse_proxy_protocol_v2_header(char *hdr, size_t len,
+  struct sockaddr_storage *addr_storage,
+  int *port, bool *local_connection)
+{
+  *local_connection = false;
+  /* V2 Signature */
+  if (memcmp(hdr, "\x0D\x0A\x0D\x0A\x00\x0D\x0A\x51\x55\x49\x54\x0A", 12))
+    return -1;
+
+  /* version  + command */
+  uint8 ver = (hdr[12] & 0xF0);
+  if (ver != 0x20)
+    return -1;
+  uint cmd = (hdr[12] & 0xF);
+
+  /* Address family */
+  uint8 fam = hdr[13];
+
+  if (cmd == 0)
+  {
+    /* LOCAL command*/
+    *local_connection = true;
+    return 0;
+  }
+
+  if (cmd != 0x01)
+  {
+    /* Not PROXY COMMAND */
+    return -1;
+  }
+
+  struct sockaddr_in *sin = (struct sockaddr_in *)addr_storage;
+  struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)addr_storage;
+  switch (fam)
+  {
+  case 0x11:  /* TCPv4 */
+    sin = (struct sockaddr_in *)addr_storage;
+    sin->sin_family = AF_INET;
+    memcpy(&(sin->sin_addr.s_addr), hdr + 16, 4);
+    *port = ((uchar)hdr[24] << 8) + (uchar)hdr[25];
+    break;
+  case 0x21:  /* TCPv6 */
+    sin6 = (struct sockaddr_in6 *)addr_storage;
+    sin6->sin6_family = AF_INET6;
+    memcpy(&(sin6->sin6_addr), hdr + 16, 16);
+    *port = ((uchar)hdr[48] << 8) + (uchar)hdr[49];
+    break;
+  default:
+    *local_connection = true;
+  }
+  return 0;
+}
+
+extern int thd_set_peer_addr(THD *thd, sockaddr_storage *addr, const char *ip, uint port);
+
+static int parse_proxy_protocol_header(NET *net)
+{
+  char hdr[108];
+  size_t hdr_len = 0;
+  sockaddr_storage addr= {0};
+  int port;
+
+  DBUG_ASSERT(!net->compress);
+  const uchar *preread_bytes = net->buff + net->where_b;
+  bool is_v1_header = !memcmp(preread_bytes, "PROX", 4);
+  bool is_v2_header = !is_v1_header && !memcmp(preread_bytes, "\x0D\x0A\x0D\x0A", 4);
+  if (!is_v1_header && !is_v2_header)
+  {
+    // not a proxy protocol header
+    return -1;
+  }
+  memcpy(hdr, preread_bytes, 4);
+  hdr_len = 4;
+
+
+  Vio *vio = net->vio;
+  bool local_connection = false;
+  if (is_v1_header)
+  {
+    while(hdr_len < sizeof(hdr))
+    {
+      long len = (long)vio_read(vio, (uchar *)hdr + hdr_len, 1);
+      if (len < 0)
+        return -1;
+      hdr_len++;
+      if (hdr[hdr_len-1] == '\n')
+        break;
+    }
+    hdr[hdr_len] = 0;
+
+    if (parse_proxy_protocol_v1_header(hdr, hdr_len,  &addr, &port, &local_connection))
+      return -1;
+  }
+  else // if (is_v2_header)
+  {
+    /* read off 16 bytes of the header*/
+    long len = vio_read(vio, (uchar *)hdr + 4, 12);
+    if (len < 0)
+      return -1;
+    // 2 last bytes are the length in network byte order of the part following header
+    ushort trail_len = (hdr[14] >> 8) + hdr[15];
+    if (trail_len > sizeof(hdr) - 16)
+      return -1;
+    len = vio_read(vio, (uchar *)hdr + 16, trail_len);
+    hdr_len = 16 + trail_len;
+    if (parse_proxy_protocol_v2_header(hdr, hdr_len,  &addr, &port, &local_connection))
+      return -1;
+  }
+  if (local_connection)
+    return 0;
+
+  char ip_string[128];
+  if (vio_get_normalized_ip_string((struct sockaddr*)&addr, sizeof(addr), ip_string, sizeof(ip_string)))
+    return -1;
+  return thd_set_peer_addr((THD *)net->thd, &addr, ip_string, port);
+}
+
 /**
   Reads one packet to net->buff + net->where_b.
   Long packets are handled by my_net_read().
@@ -850,6 +1032,9 @@ my_real_read(NET *net, size_t *complen,
 #ifndef NO_ALARM
   ALARM alarm_buff;
 #endif
+
+retry:
+
   my_bool net_blocking=vio_is_blocking(net->vio);
   uint32 remain= (net->compress ? NET_HEADER_SIZE+COMP_HEADER_SIZE :
 		  NET_HEADER_SIZE);
@@ -1081,6 +1266,11 @@ end:
 
 packets_out_of_order:
   {
+    if (!parse_proxy_protocol_header(net))
+    {
+      goto retry;
+    }
+
     DBUG_PRINT("error",
                ("Packets out of order (Found: %d, expected %u)",
                 (int) net->buff[net->where_b + 3],
